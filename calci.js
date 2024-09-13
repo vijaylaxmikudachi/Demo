d771f16950b223d42c007d34e3c1a93dfa00df48
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-13 21:49:17 +0530

    Add divide function

--- calci.js
+++ calci.js
@@ -2,4 +2,22 @@
 function add(a, b) {
     return a + b;
 }
-console.log(add(5,5));
+console(add(10,5));
+
+function subtract(a, b) {
+    return a - b;
+}
+console(subtract(10,5));
+
+function multiply(a, b) {
+    return a * b;
+}
+console(multiply(10,5));
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Cannot divide by zero!";
+    }
+    return a / b;
+}
+console(divide(10,5));
