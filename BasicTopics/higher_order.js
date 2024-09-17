183d71fb8dbb1a96eaabfd9a1afa91655c9d8c7e
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-17 18:05:40 +0530

    [vijaylaxmi]Added:higher_order files

--- BasicTopics/higher_order.js
+++ BasicTopics/higher_order.js
@@ -14,3 +14,20 @@ function x(){
 function y(){
     x();
 }
+
+//Example3
+const radius = [3,2,1,5];
+const area = function (radius){ return Math.PI*radius*radius};
+const cf = function (radius){ return 2*Math.PI*radius};
+const dia = function (radius){ return 2*radius};
+
+const calculate = function(radius,logic){
+    const op = [];
+    for (let i=0;i<radius.length;i++){
+        op.push(logic(radius[i]));
+    }
+    return op;
+}
+console.log(calculate(radius,area));
+console.log(calculate(radius,cf));
+console.log(calculate(radius,dia));
