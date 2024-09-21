e5eecc0b2fad70164cd3c09e1ebe3297f8a31e15
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-21 08:41:40 +0530

    [vijaylaxmi]Added:Daily Wages of Emp

--- EmpWageSys/WelcomToEWS.js
+++ EmpWageSys/WelcomToEWS.js
@@ -8,3 +8,15 @@ if (attendance === 1) {
 } else {
   console.log("Employee is Absent");
 }
+
+//Daily wages of emp
+const WAGE_PER_HOUR = 20;
+const FULL_DAY_HOURS = 8;
+
+// Calculate daily wage based on attendance
+if (attendance === 1) {
+  const dailyWage = FULL_DAY_HOURS * WAGE_PER_HOUR;
+  console.log("Daily Wage:", dailyWage);
+} else {
+  console.log("Daily Wage: 0 (Employee is Absent)");
+}
