d711ff57c0b4a5454fda8459d1d7998a4f150b41
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-09-21 08:37:59 +0530

    [vijaylaxmi]Added:Attendance

--- EmpWageSys/WelcomToEWS.js
+++ EmpWageSys/WelcomToEWS.js
@@ -1,1 +1,10 @@
 console.log("Welcome to Employee Wage Computation Program");
+//random number for attendance (0 for absent, 1 for present)
+const attendance = Math.floor(Math.random() * 2);
+
+// Check attendance 
+if (attendance === 1) {
+  console.log("Employee is Present");
+} else {
+  console.log("Employee is Absent");
+}
